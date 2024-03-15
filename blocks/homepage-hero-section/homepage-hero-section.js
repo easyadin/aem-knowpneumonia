06c56b19ca8d0613265d916a3df33c1ae96d2cf9
Israel Ayokunnu <38165966+easyadin@users.noreply.github.com>
2024-03-15 05:07:03 +0400

    wip: index hero section

---
+++ blocks/homepage-hero-section/homepage-hero-section.js
@@ -0,0 +1,9 @@
+import { getMetadata } from '../../scripts/aem.js';
+import desktopHeaderMenuNavigation from '../desktop-header-menu-navigation/desktop-header-menu-navigation.js';
+import { loadFragment } from '../fragment/fragment.js';
+
+/**
+ * Loads and Decorates the header
+ * @param {Element} block The header block element
+ */
+export default async function decorate(block) {}
