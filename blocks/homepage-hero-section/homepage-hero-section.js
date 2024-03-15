b6a2a791acb35241a627889582dc520e963f79a5
Israel Ayokunnu <38165966+easyadin@users.noreply.github.com>
2024-03-15 14:26:17 +0400

    wip: homepage hero section

--- blocks/homepage-hero-section/homepage-hero-section.js
+++ blocks/homepage-hero-section/homepage-hero-section.js
@@ -1,9 +1,43 @@
-import { getMetadata } from '../../scripts/aem.js';
-import desktopHeaderMenuNavigation from '../desktop-header-menu-navigation/desktop-header-menu-navigation.js';
-import { loadFragment } from '../fragment/fragment.js';
-
 /**
- * Loads and Decorates the header
- * @param {Element} block The header block element
+ * Loads and Decorates the homepage hero section
+ * @param {Element} block The homepage hero section block element
  */
-export default async function decorate(block) {}
+export default async function decorate(block) {
+  const slides = block.querySelectorAll(
+    '.homepage-hero-section.block > div > div:nth-child(2) > ul > li'
+  );
+
+  const sliderNav = document.createElement('div');
+  sliderNav.className = 'homepage-hero-section-slider-nav-button-wrapper';
+
+  Array.from(slides).forEach((slide, index) => {
+    const button = document.createElement('button');
+    button.className = `homepage-hero-section-slider-nav-button slide-${
+      index + 1
+    }`;
+    button.addEventListener('click', () => {
+      slides.forEach((s) => s.classList.remove('active'));
+      Array.from(sliderNav.children).forEach((b) =>
+        b.classList.remove('active')
+      );
+
+      slide.classList.add('active');
+      button.classList.add('active');
+
+      slides.forEach((s) => (s.style.display = 'none'));
+      slide.style.display = 'block';
+    });
+
+    sliderNav.appendChild(button);
+  });
+
+  block.append(sliderNav);
+
+  if (slides.length > 0) {
+    slides[0].classList.add('active');
+    slides[0].style.display = 'block';
+  }
+  if (sliderNav.children.length > 0) {
+    sliderNav.children[0].classList.add('active');
+  }
+}
