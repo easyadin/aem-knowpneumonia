7f9f34cdef4314ecf68fbc59f86b154ba1b46a8b
Israel Ayokunnu <38165966+easyadin@users.noreply.github.com>
2024-03-26 13:38:28 +0400

    ui: angled block

--- blocks/angled-hero-information/angled-hero-information.js
+++ blocks/angled-hero-information/angled-hero-information.js
@@ -2,4 +2,14 @@
  *
  * @param {Element} block
  */
-export default function decorate(block) {}
+export default function decorate(block) {
+  const angledBlocks = block.querySelectorAll(
+    '.angled-hero-information > div:last-child div'
+  );
+
+  let zIndex = angledBlocks.length;
+
+  angledBlocks.forEach((element) => {
+    element.style.zIndex = zIndex--;
+  });
+}
