9b0a3f59f00554c1dbda34b7f1f80549edfbdfef
Israel Ayokunnu <38165966+easyadin@users.noreply.github.com>
2024-03-26 18:03:13 +0400

    ui true false block

--- blocks/true-false-questionnaire/true-false-questionnaire.js
+++ blocks/true-false-questionnaire/true-false-questionnaire.js
@@ -2,4 +2,35 @@
  *
  * @param {Element} block
  */
-export default function decorate(block) {}
+export default function decorate(block) {
+  const questionElement = block.querySelector(
+    '.true-false-questionnaire > div:first-child'
+  );
+  const trueElement = block.querySelector(
+    '.true-false-questionnaire > div:first-child div:nth-child(2)'
+  );
+  const falseElement = block.querySelector(
+    '.true-false-questionnaire > div:first-child div:nth-child(3)'
+  );
+  const trueAnswer = block.querySelector(
+    '.true-false-questionnaire > div:nth-child(2)'
+  );
+  const falseAnswer = block.querySelector(
+    '.true-false-questionnaire > div:nth-child(3)'
+  );
+
+  trueAnswer.style.display = 'none';
+  falseAnswer.style.display = 'none';
+
+  trueElement.addEventListener('click', () => {
+    questionElement.style.display = 'none';
+    trueAnswer.style.display = 'block';
+    falseAnswer.style.display = 'none';
+  });
+
+  falseElement.addEventListener('click', () => {
+    questionElement.style.display = 'none';
+    falseAnswer.style.display = 'block';
+    trueAnswer.style.display = 'none';
+  });
+}
